ea5b0d23ad848a4fde8cad75b8c3bdbd93b60bb1
Kev <kchung93@uw.edu>
2022-05-09 19:24:41 -0700

    learn: api practice

--- flask-web/app/nps_api.py
+++ flask-web/app/nps_api.py
@@ -1,13 +1,19 @@
 import requests
 
 API_KEY = "rRScznr5cMqmr00eoeO61Xmc3FL9fB6o499OJqbf"
+STATE = "id"
 
-endpoint = f"https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key={API_KEY}"
+# endpoint = f"https://developer.nps.gov/api/v1/parks?stateCode={STATE}&api_key={API_KEY}"
+endpoint = f"https://developer.nps.gov/api/v1/webcams?limit=1000&api_key={API_KEY}"
+
 
 response = requests.get(endpoint)
 data = response.json()
-wa_parks = data["data"]
+parks = data["data"]
 
-for idx, name in enumerate(wa_parks):
-    print(f'Location: {wa_parks[idx]["latLong"]}')
-    print(f'{wa_parks[idx]["fullName"]}\n')
+
+park_code = {}
+for entry in parks:
+    park_code[entry["parkCode"]] = (entry["states"], entry["fullName"])
+
+print(park_code)
