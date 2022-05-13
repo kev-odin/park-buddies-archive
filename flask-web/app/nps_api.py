87bc610d3eb050685e90779a87af11639399462f
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2022-05-12 18:47:47 -0700

    Playing around API

--- flask-web/app/nps_api.py
+++ flask-web/app/nps_api.py
@@ -1,15 +1,18 @@
 import requests
 
 # API_KEY = "rRScznr5cMqmr00eoeO61Xmc3FL9fB6o499OJqbf"
+# make request to session token 
 API_KEY = "P8tQHRp3dvGMLzBQeXOwHQmi456Eyo3zgGpYI8Gw"
 STATE = "id"
 
 # endpoint = f"https://developer.nps.gov/api/v1/parks?stateCode={STATE}&api_key={API_KEY}"
-endpoint = f"https://developer.nps.gov/api/v1/webcams?limit=1000&api_key={API_KEY}"
+target = "webcams"
+endpoint = f"https://developer.nps.gov/api/v1/"+target+f"?limit=3&api_key={API_KEY}"
 
 
 response = requests.get(endpoint)
 data = response.json()
+print(data)
 parks = data["data"]
 
 
@@ -17,4 +20,4 @@ park_code = {}
 for entry in parks:
     park_code[entry["parkCode"]] = (entry["states"], entry["fullName"])
 
-print(park_code)
+# print(park_code)
