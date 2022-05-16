e10057985d800494b689bfa614d5df9db4acb607
Kev <kchung93@uw.edu>
2022-05-16 14:32:42 -0700

    docs: fixed typo

--- flask-web/nps_api.py
+++ flask-web/nps_api.py
@@ -4,6 +4,10 @@ base_url = f"https://developer.nps.gov/api/v1/"
 params = {"api_key": "rRScznr5cMqmr00eoeO61Xmc3FL9fB6o499OJqbf"}
 
 
+def _address_string(addresses: dict):
+    return f"{addresses[0]['line1']}, {addresses[0]['city']}, {addresses[0]['stateCode']} {addresses[0]['postalCode']}"
+
+
 def activities():
     """
     Returns list of activities that can be enjoyed at every park
@@ -11,7 +15,8 @@ def activities():
     """
     request_url = base_url + "activities"
     response = requests.get(request_url, params=params)
-    activites = [x["name"] for x in response.json()["data"]]
+    data = response.json()["data"]
+    activites = [x["name"] for x in data]
     return activites
 
 
@@ -35,18 +40,15 @@ def webcams():
 def parks(state_code: str = "WA"):
     """
     Returns a list of parks in a provided state
-    [(state_name, park_code, park_address, park_latLong)]
+    [(state_name, park_code, park_address, park_images)]
     """
     request_url = base_url + "parks"
     params["stateCode"] = state_code
     response = requests.get(request_url, params=params)
     data = response.json()["data"]
     parks = [
-        (x["fullName"], x["parkCode"], _address_string(x["addresses"]), x["latLong"])
+        (x["fullName"], x["parkCode"], _address_string(x["addresses"]), x["images"])
         for x in data
     ]
     return parks
 
-
-def _address_string(addresses: dict):
-    return f"{addresses[0]['line1']}, {addresses[0]['city']}, {addresses[0]['stateCode']} {addresses[0]['postalCode']}"
