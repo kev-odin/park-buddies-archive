5cdedc4704fca26ba2f3bb49dde424f88c9deb8a
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2022-05-16 12:13:46 -0700

    "before merging kevc's current work"

--- flask-web/app/nps_api.py
+++ flask-web/app/nps_api.py
@@ -6,18 +6,19 @@ API_KEY = "P8tQHRp3dvGMLzBQeXOwHQmi456Eyo3zgGpYI8Gw"
 STATE = "id"
 
 # endpoint = f"https://developer.nps.gov/api/v1/parks?stateCode={STATE}&api_key={API_KEY}"
-target = "webcams"
-endpoint = f"https://developer.nps.gov/api/v1/"+target+f"?limit=3&api_key={API_KEY}"
+target = "webcams" # API category to feed to endpoint
+lmt = 10 # returned number limits to feed to endpoint
+endpoint = f"https://developer.nps.gov/api/v1/"+target+f"?limit=lmt&api_key={API_KEY}"
 
 
 response = requests.get(endpoint)
 data = response.json()
-print(data)
 parks = data["data"]
 
 
-park_code = {}
-for entry in parks:
-    park_code[entry["parkCode"]] = (entry["states"], entry["fullName"])
+
+# park_code = {}
+# for entry in parks:
+#     park_code[entry["parkCode"]] = (entry["states"], entry["fullName"])
 
 # print(park_code)
