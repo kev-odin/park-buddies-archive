1bf659e408d2db00c4d2b9eb39a4e8c93465aaa1
Kev <kchung93@uw.edu>
2022-05-16 14:04:47 -0700

    feat: api for activities, webcams, parks_info

--- flask-web/nps_api.py
+++ flask-web/nps_api.py
@@ -1,21 +1,52 @@
 import requests
 
-API_KEY = "?api_key=rRScznr5cMqmr00eoeO61Xmc3FL9fB6o499OJqbf"
-
 base_url = f"https://developer.nps.gov/api/v1/"
+params = {"api_key": "rRScznr5cMqmr00eoeO61Xmc3FL9fB6o499OJqbf"}
 
-response = requests.get(base_url)
 
+def activities():
+    """
+    Returns list of activities that can be enjoyed at every park
+    [activity_1, ...]
+    """
+    request_url = base_url + "activities"
+    response = requests.get(request_url, params=params)
+    activites = [x["name"] for x in response.json()["data"]]
+    return activites
 
-def activity_list():
-    activities_url = base_url + f"activities{API_KEY}"
-    response = requests.get(activities_url)
-    return response.json() 
-
-print(activity_list())
 
 def webcams():
-    pass
+    """
+    Returns list of active webcams at each park
+    [(webcam_title, url_link, related_parks)]
+    """
+    params["limit"] = 500
+    request_url = base_url + "webcams"
+    response = requests.get(request_url, params=params)
+    data = response.json()["data"]
+    webcams = [
+        (x["title"], x["url"], x["relatedParks"])
+        for x in data
+        if x["status"] == "Active"
+    ]
+    return webcams
 
-def parks_info():
-    pass
+
+def parks(state_code: str = "WA"):
+    """
+    Returns a list of parks in a provided state
+    [(state_name, park_code, park_address, park_latLong)]
+    """
+    request_url = base_url + "parks"
+    params["stateCode"] = state_code
+    response = requests.get(request_url, params=params)
+    data = response.json()["data"]
+    parks = [
+        (x["fullName"], x["parkCode"], _address_string(x["addresses"]), x["latLong"])
+        for x in data
+    ]
+    return parks
+
+
+def _address_string(addresses: dict):
+    return f"{addresses[0]['line1']}, {addresses[0]['city']}, {addresses[0]['stateCode']} {addresses[0]['postalCode']}"
