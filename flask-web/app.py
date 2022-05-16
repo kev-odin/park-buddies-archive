0a0f659b1c7a0d9d0d5b8bbc48c1552cd11a7a2e
Kev <kchung93@uw.edu>
2022-05-16 14:05:05 -0700

    format: back formatted changes

--- flask-web/app.py
+++ flask-web/app.py
@@ -96,12 +96,12 @@ def logout():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(403)
 def access_denied(e):
-    return render_template('403.html'), 403
+    return render_template("403.html"), 403
 
 
 if __name__ == "__main__":
