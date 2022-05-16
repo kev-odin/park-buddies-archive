2f6031229f9c5a0bd5aee47a0d0bdbc89ab6cfb5
Kev <kchung93@uw.edu>
2022-05-15 22:02:09 -0700

    feat: add 404 and 403 render html

--- flask-web/app.py
+++ flask-web/app.py
@@ -94,5 +94,15 @@ def logout():
     return redirect("/login")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def access_denied(e):
+    return render_template('403.html'), 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
