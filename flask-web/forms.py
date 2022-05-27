e3b86eab802396aa1b55014e29bdb5de448f4765
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2022-05-27 07:59:32 -0700

    aligned the position of form

--- flask-web/forms.py
+++ flask-web/forms.py
@@ -34,7 +34,7 @@ class RegisterForm(FlaskForm):
 
 class searchForm(FlaskForm):
     # state = SelectField('Choose a state!', choices=['WA', 'OR', 'CA'], validators=[DataRequired()])
-    state = SelectField("Select state", choices=STATE_LIST, validators=[DataRequired()]) 
+    state = SelectField("Select state for details", choices=STATE_LIST, validators=[DataRequired()]) 
     submit = SubmitField(label="Search")
 
 class SettingsForm(FlaskForm):
